dd4cd48d18752e7da6635347fe5ef986b1e0e061
Alfian Rehanusa Wibowo <alfian@mit.id>
2021-05-31 15:12:28 +0700

    updatePost()

--- main.go
+++ main.go
@@ -114,3 +114,31 @@ func getPost(w http.ResponseWriter, r *http.Request) {
 
 	json.NewEncoder(w).Encode(post)
 }
+
+func updatePost(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	stmt, err := db.Prepare("UPDATE posts SET title = ? WHERE id = ?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+
+	newTitle := keyVal["title"]
+	_, err = stmt.Exec(newTitle, params["id"])
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Post with ID = %s was updated", params["id"])
+}

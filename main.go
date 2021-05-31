dd4c962569a2bbe77373e602639c71ce2f229e36
Alfian Rehanusa Wibowo <alfian@mit.id>
2021-05-31 15:13:18 +0700

    deletePost()

--- main.go
+++ main.go
@@ -142,3 +142,22 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Post with ID = %s was updated", params["id"])
 }
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+
+	stmt, err := db.Prepare("DELETE FROM posts WHERE id = ?")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_, err = stmt.Exec(params["id"])
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
+}

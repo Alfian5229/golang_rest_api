80a99f8b37920f12373fc33f602ced07d72f0d32
Alfian Rehanusa Wibowo <alfian@mit.id>
2021-05-31 15:11:54 +0700

    getPost()

--- main.go
+++ main.go
@@ -90,3 +90,27 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "New post was created")
 }
+
+func getPost(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)
+	result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", params["id"])
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+	var post Post
+
+	for result.Next() {
+		err := result.Scan(&post.ID, &post.Title)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	json.NewEncoder(w).Encode(post)
+}

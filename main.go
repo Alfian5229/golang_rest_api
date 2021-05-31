3fffb9969df7b20a337aa844a28818a5a4864f8f
Alfian Rehanusa Wibowo <alfian@mit.id>
2021-05-31 14:16:25 +0700

    Initialising the router

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 var db *sql.DB
@@ -11,8 +13,13 @@ var err error
 
 func main() {
 	db, err = sql.Open("mysql", "alfian:laptopasus5229@tcp(127.0.0.1:3306)/golang")
+
 	if err != nil {
 		panic(err.Error())
 	}
+
 	defer db.Close()
+
+	router := mux.NewRouter()
+	http.ListenAndServe(":8000", router)
 }
